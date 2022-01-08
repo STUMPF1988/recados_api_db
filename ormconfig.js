ad71e32e7e262af764b4d783a65ea36d67f33c43
STUMPF1988 <stumpfdaniele@gmail.com>
2022-01-08 20:16:10 -0300

    quarto_commit

--- ormconfig.js
+++ ormconfig.js
@@ -10,14 +10,15 @@ module.exports = {
     url: process.env.DATABASE_URL,
     synchronize: false,
     logging: false,
-    entities:[ process.env.ENTITIES_DIR],
-    cli: {
-    entitiesDir: 'src/core/database/entities',
-    migrationsDir: 'src/core/database/migrations'
-    },
-    migrations: [
-        'src/core/database/migrations/**/*.ts'
+    entities:[ process.env.ENTITIES_DIR
     ],
+    migrations: [
+        process.env.MIGRATIONS_DIR
+    ],
+    cli: {
+        entitiesDir: 'src/core/database/entities',
+        migrationsDir: 'src/core/database/migrations'
+        },
     subscribers: [],
     extra: {
         ssl: {
